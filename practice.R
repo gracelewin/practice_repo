e61f55f6a92bd610d3cdb0c8cc96e7de56175abf
gracelewin <glewin@bren.ucsb.edu>
2025-05-15 10:39:41 -0700

    taking out legend and using different theme

--- practice.R
+++ practice.R
@@ -5,4 +5,6 @@ ggplot(pie_crab, aes(x = latitude, y = size, color = latitude)) +
   geom_point() +
   scale_color_gradient(low = "blue", high = "orange") +
   labs(x = "Latitude",
-       y = "Crab carapace width (mm)")
+       y = "Crab carapace width (mm)")+
+  theme_bw() +
+  theme(legend.position = "none")
